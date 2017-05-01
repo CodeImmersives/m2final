3c12a60e408d0a1faf612184f816308d6628d79b
JediMindBang <quietprosf@gmail.com>
2017-05-01 10:42:07 -0400

    redesign
    I had issues with the CSS as stated, I made the site look a bit more attractive with a multi page site and some simplistic design.

---
+++ finalReDesign/public/js/mainpage.js
@@ -0,0 +1,56 @@
+var allSelector = document.getElementById("all");
+
+function displayQuestions(qaItems) {
+    var questionBox = document.getElementById("question-box");
+
+    for (var i = 0; i < qaItems.length; i++) {
+        var checkBox = document.createElement("INPUT");
+        var header = document.createElement("h3");
+        var sentence = document.createElement("p");
+        var answer = document.createElement("button");
+        
+        checkBox.setAttribute("type", "checkbox");        
+        header.innerText = 'Question' + qaItems[i].id;
+        sentence.innerText = ':' + qaItems[i].question;
+        answer.innerText  = 'Answer!';
+
+        questionBox.appendChild(header);
+        header.appendChild(checkBox);
+        questionBox.appendChild(answer);
+        questionBox.appendChild(sentence);
+    };
+    
+    // error this function(let's check it out later)
+    checkBox.onchange = function() {
+        if (this.checked) {
+            this.questionBox.className = "hide";
+            header.removeChild(checkBox);
+            questionBox.removeChild(answer);
+            questionBox.removeChild(sentence);
+        };
+    }
+
+}
+    
+
+
+var baseurl = "http://localhost:3000";
+
+function getAll() {                
+    var xhttp = new XMLHttpRequest();
+    var url = baseurl + "/qas"
+      xhttp.onreadystatechange = function() {
+       if (this.readyState == 4 && this.status == 200) {
+           var questions = JSON.parse(this.responseText);
+           console.log(questions);
+           displayQuestions(questions);
+        }
+      };
+      xhttp.open("GET", url, true);
+      xhttp.send();                
+};
+
+
+
+
+allSelector.onclick = getAll;
